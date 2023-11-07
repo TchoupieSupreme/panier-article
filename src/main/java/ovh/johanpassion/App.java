9e15782b21ea5cad0c36bbbffee3230ab93c35c4
TchoupieSupreme <passion.johan@gmail.com>
2023-11-07 09:15:11 +0100

    first commit

---
+++ src/main/java/ovh/johanpassion/App.java
@@ -0,0 +1,21 @@
+package ovh.johanpassion;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        Double prixTotal = 0.0;
+        System.out.println("Combien d'article avez-vous ?");
+        int nbArticle = clavier.nextInt();
+        for (int i=0;i<nbArticle;i++){
+            System.out.println("Donner le prix de l'article "+i);
+            Double prix = clavier.nextDouble();
+            prixTotal += prix;
+        }
+        System.out.println("Votre total est de : "+prixTotal);
+        clavier.close();
+    }
+    
+}
